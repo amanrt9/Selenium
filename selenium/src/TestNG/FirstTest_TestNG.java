bcb923f7db867b28489dc9503e1c5c3251263826
AmanTekade <tekale345@gmail.com>
2024-11-08 15:05:38 +0530

    Third push

---
+++ selenium/src/TestNG/FirstTest_TestNG.java
@@ -0,0 +1,31 @@
+package TestNG;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class FirstTest_TestNG {
+	    @Test
+	    public void TC03_Print2()
+	    {
+	    	Reporter.log("Heyy",true); // when true print in emailable report and console
+	    	                           //and if write false then only write in emailable report
+	    }
+	    
+	    @Test
+	    public void TC01_openGoogle()
+	    {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+	    }
+	    
+	    
+	    @Test
+	    public void TC02_print()
+	    {
+          Reporter.log("Hello From TestNG", true);
+	    }
+	
+
+}
--- selenium/src/pom_With_DDF/SwagLabLoginTest.java
+++ selenium/src/pom_With_DDF/SwagLabLoginTest.java
@@ -61,5 +61,6 @@ public class SwagLabLoginTest {
 		driver.quit();
 
 	}
+	
 
 }
